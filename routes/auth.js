0dc232f84bdc22357198704b483e5589502fc160
happdev0 <happydev0705@gmail.com>
2023-08-09 05:13:50 -0400

    second

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/auth");
-
-// router.post('/signin', authMiddelware.authJwt);
-router.post('/signup', controller.signup);
-router.post('/signin', controller.signin);
-
-module.exports = router;
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/user");
-
-// router.post('/signin', authMiddelware.authJwt);
-router.get('/', controller.getUsers);
-// router.post('/signin', controller.signin);
-
-module.exports = router;
